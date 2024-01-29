ef87257e9409c3c439fea45a4ce2f939bdb32ab1
Alexey Frolov <frolov.git@mail.ru>
2024-01-30 02:11:13 +0300

    redKaty1 @RequestParam(required=false)String параметр_запроса

--- src/main/java/com/example/webka_1/controllers/RedKattyController.java
+++ src/main/java/com/example/webka_1/controllers/RedKattyController.java
@@ -3,14 +3,17 @@ package com.example.webka_1.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RedKattyController {
 
-    @RequestMapping("/homew")
-    public String home(Model model) {
-        model.addAttribute("username", "Katy");
-        model.addAttribute("color", "RED");
-        return "homew.html";
+    @RequestMapping("/homeW")
+    public String home(Model model,
+                       @RequestParam(required = false)String color,//это параметр запроса, uri должно быть:http://localhost:8080/home?color=red
+                       @RequestParam(required = false)String name) {//это еще параметр запроса, теперь uri должно быть:http://localhost:8080/home?color=цвет?name=имя
+        model.addAttribute("username", name);//в представлении:<span th:style="'color:' + ${color}"  th:text="${username}"></span>
+        model.addAttribute("color", color);//
+        return "homeW.html";
     }
 }
