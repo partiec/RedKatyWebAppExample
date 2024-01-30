e139d1b55002fa2d848046b92365bf1074a4ce8c
Alexey Frolov <frolov.git@mail.ru>
2024-01-30 03:44:26 +0300

    @PathVariable

--- src/main/java/com/example/webka_1/controllers/RedKattyController.java
+++ src/main/java/com/example/webka_1/controllers/RedKattyController.java
@@ -2,6 +2,7 @@ package com.example.webka_1.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,5 +16,14 @@ public class RedKattyController {
         model.addAttribute("username", name);//в представлении:<span th:style="'color:' + ${color}"  th:text="${username}"></span>
         model.addAttribute("color", color);//
         return "homeW.html";
+    }
+
+    @RequestMapping("/home2/{cvet}")// Запрос должен быть: http://home2/цвет  цвет будет считан из url в @PathVariable String cvet.
+    public String home2(Model model,
+                        @PathVariable String cvet){
+
+        model.addAttribute("colorView", cvet);
+        model.addAttribute("nameView", "Katy_vtoraya");
+        return "view2.html";
     }
 }
