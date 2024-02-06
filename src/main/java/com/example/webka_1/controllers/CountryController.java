83e1abc27c309f2ae138cbfdd21c29d2314f2d09
Alexey Frolov <frolov.git@mail.ru>
2024-02-06 12:08:13 +0300

    response body

--- src/main/java/com/example/webka_1/controllers/CountryController.java
+++ src/main/java/com/example/webka_1/controllers/CountryController.java
@@ -1,6 +1,8 @@
 package com.example.webka_1.controllers;
 
 import com.example.webka_1.models.Country;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,16 @@ public class CountryController {
         l.add(Country.of(france().getName(), france().getPopulation()));
         l.add(Country.of("Russia", 300));
         return l;
+    }
+
+    @GetMapping("/france_c")
+    public ResponseEntity<Country> france_c(){
+        Country c = Country.of("France", 44);
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .header("continent", "Europe")
+                .header("capital", "Paris")
+                .header("favorite_food", "cheese and wine")
+                .body(c);
     }
 }
